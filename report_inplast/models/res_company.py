bea05cce97ddc858b36d7e5a91c88e1d9d242f18
arquero20022 <60667813+arquero20022@users.noreply.github.com>
2024-05-13 12:08:49 +0200

    Add files via upload

--- report_inplast/models/res_company.py
+++ report_inplast/models/res_company.py
@@ -4,3 +4,7 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     pnt_goods_state_table = fields.Binary("State Table verification")
+    pnt_logo_cert_uno = fields.Binary("logo cert uno")
+    pnt_logo_cert_dos = fields.Binary("logo cert dos")
+    pnt_logo_cert_tres = fields.Binary("logo cert tres")
+
