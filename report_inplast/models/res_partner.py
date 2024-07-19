d1bb59a997e7686a913036be36d8af3a02119ade
amunoz <amunoz@puntsistemes.es>
2024-07-19 10:08:40 +0000

    actualizacion

--- report_inplast/models/res_partner.py
+++ report_inplast/models/res_partner.py
@@ -6,5 +6,6 @@ class ResPartner(models.Model):
     #pnt_sale_report_id = fields.Many2one('ir.ui.view', domain=[("priority", "=", "20")])
     #pnt_invoice_report_id = fields.Many2one('ir.ui.view', domain=[("priority", "=", "21")])
     pnt_picking_report_id = fields.Many2one('picking.report.type')
+
     #pnt_label_box_type_id = fields.Many2one('ir.ui.view', domain=[("priority", "=", "23")])
     #pnt_label_pallet_type_id = fields.Many2one('ir.ui.view', domain=[("priority", "=", "24")])
